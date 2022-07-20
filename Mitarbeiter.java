78b8a433a2e0803e0d913769ea86bb069c343435
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-07-20 11:20:12 +0200

    first commit

---
+++ Mitarbeiter.java
@@ -0,0 +1,17 @@
+public abstract class Mitarbeiter {
+    private String personalNummer;
+    private String vorname;
+    private String name;
+    private double grundgehalt = 2000.0;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrundgehalt() {
+        return this.grundgehalt;
+    }
+
+    public abstract double monatsBrutto();
+
+}
